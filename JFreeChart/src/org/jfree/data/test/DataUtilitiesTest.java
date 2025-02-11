11f23cac4b64e699fa7aa0cc92637f83feee2297
austenzhang <austengzhang@gmail.com>
2025-02-11 15:13:58 -0700

    Implement all test cases for calculateColumnTotal() method

--- JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
+++ JFreeChart/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -2,6 +2,8 @@ package org.jfree.data.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.security.InvalidParameterException;
+
 import org.jfree.data.DataUtilities;
 import org.jfree.data.Values2D;
 import org.jmock.Expectations;
@@ -40,6 +42,36 @@ public class DataUtilitiesTest extends DataUtilities {
 
 		double result = DataUtilities.calculateColumnTotal(mockData, 1);
 		assertEquals(6.0, result, 0.0001);
+	}
+
+	// Test Case 2: Null Values2D data object
+	@Test(expected = InvalidParameterException.class)
+	public void testInvalidDataAndValidColumn() {
+		DataUtilities.calculateColumnTotal(null, 0);
+	}
+
+	// Test Case 3:Valid Values2D data object but invalid column index (negative)
+	@Test
+	public void testValidDataAndInvalidColumn() {
+		context.checking(new Expectations() {
+			{
+				allowing(mockData).getRowCount();
+				will(returnValue(2));
+				allowing(mockData).getColumnCount();
+				will(returnValue(2));
+				allowing(mockData).getValue(0, 1);
+				will(returnValue(2.0));
+				allowing(mockData).getValue(1, 1);
+				will(returnValue(4.0));
+				allowing(mockData).getValue(0, 0);
+				will(returnValue(1.0));
+				allowing(mockData).getValue(1, 0);
+				will(returnValue(3.0));
+			}
+		});
+
+		double result = DataUtilities.calculateColumnTotal(mockData, -10);
+		assertEquals(0.0, result, 0.0001);
 	}
 
 	@After
